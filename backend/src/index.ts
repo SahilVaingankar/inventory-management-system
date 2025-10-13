acd70f0bab66575ddac9e4872be1aa6e9033a4d2
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-10-13 12:48:49 +0530

    fixed url typo on backend/index origins array

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 50000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://invertory-management-system.vercel.app/",
+  "https://invertory-management-system.vercel.app",
 ];
 
 app.use(express.json());
