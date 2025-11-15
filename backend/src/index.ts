e7ffb946352d2d4be6f4bfa5b363b0e4d400c19a
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-11-15 11:39:27 +0530

    fixed url typo in allowed origins in backend/index.ts file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 50000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://inventory-management-system-three-xi.vercel.app/",
+  "https://inventory-management-system-three-xi.vercel.app",
 ];
 
 app.use(express.json());
